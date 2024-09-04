b02f63ec8200131a0b3fc4086cd4a8b2c2ecc27c
Bilal Aurangzeb Khan <78090865+bilalaurangzebkhan@users.noreply.github.com>
2024-09-05 02:09:28 +0500

    Testimonials added

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -55,7 +55,7 @@ const pricingTiers = [
 
 export const Pricing = () => {
   return (
-    <section className="py-24">
+    <section className="py-24 bg-white">
       <div className="container">
         <div className="section-heading">
           <h2 className="section-title">Pricing</h2>
@@ -72,7 +72,7 @@ export const Pricing = () => {
             features 
           }) => (
             <div className={twMerge(
-              "p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px_14px_#EAEAEA] max-w-xs w-full", 
+              "card", 
               inverse === true && 'bg-black border-black text-white/60')}
             >
               <div className='flex justify-between'>
