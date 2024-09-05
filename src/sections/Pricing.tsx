dcfd999d0cc93d7da6797398b058a1bc749b47cd
Bilal Aurangzeb Khan <78090865+bilalaurangzebkhan@users.noreply.github.com>
2024-09-05 16:39:44 +0500

    Update Pricing.tsx

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -118,8 +118,8 @@ export const Pricing = () => {
                 {buttonText} 
               </button>
               <ul className='flex flex-col gap-5 mt-8'>
-                {features.map((feature) => (
-                  <li className='text-sm flex items-center gap-4'>
+                {features.map((feature, index) => (
+                  <li key={index} className='text-sm flex items-center gap-4'>
                     <CheckIcon className="h-6 w-6" />
                     <span> {feature} </span>
                   </li>
